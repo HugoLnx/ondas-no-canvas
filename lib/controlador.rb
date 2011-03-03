092da5c11490da2484973f1eefed0c7ffdbe9587
HugoLnx <hugolnx@gmail.com>
2011-03-02 21:24:44 -0300

    vizualizacao de erro

--- lib/controlador.rb
+++ lib/controlador.rb
@@ -5,11 +5,16 @@ module OndasNoCanvas
     end
   
     post '/gera_onda' do
-      onda = OndasNoCanvas::Modelos::Onda.new params
-      @delay = 10
-      @ys = onda.gerar_todos_os_ys :delay => @delay,
-                                   :x_max => 500
-      erb :'gera_onda.js'
+      begin
+        onda = OndasNoCanvas::Modelos::Onda.new params
+        @delay = 10
+        @ys = onda.gerar_todos_os_ys :delay => @delay,
+                                     :x_max => 500
+        erb :'gera_onda.js'
+      rescue OndasNoCanvas::Modelos::Infra::OndaException => exception
+        @mensagem_de_erro = exception.mensagem_notificacao
+        erb :'mostrar_erro.js'
+      end
     end
   end
 end
